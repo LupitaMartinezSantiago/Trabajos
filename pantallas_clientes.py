a7145103132ea4db6b0f2414481835e7fb60d230
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-26 21:14:42 -0600

    Update pantallas_clientes.py

--- pantallas_clientes.py
+++ pantallas_clientes.py
@@ -7,11 +7,11 @@ from mss import mss
 def capture_and_send_screen(conn):
     with mss() as sct:
         while True:
-            # Captura toda la pantalla
+         
             screen = sct.grab(sct.monitors[0])
             # Convierte la captura en un array de NumPy
             screen_np = np.array(screen)
-            # Codifica la imagen como JPEG
+         
             _, frame = cv2.imencode('.jpg', screen_np)
             data = frame.tobytes()
             msg_size = struct.pack(">L", len(data))
@@ -19,7 +19,7 @@ def capture_and_send_screen(conn):
 
 def client_program():
     host = '172.168.3.104'
-    port = 5001  # Usa el mismo puerto que en el servidor
+    port = 5001  
     client_socket = socket.socket()
     client_socket.connect((host, port))
     capture_and_send_screen(client_socket)
