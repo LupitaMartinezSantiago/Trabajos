df6a5ad71c12078db015c41dee06a1e4c1655752
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-26 21:15:44 -0600

    Update permitirping.py

--- permitirping.py
+++ permitirping.py
@@ -31,10 +31,10 @@ def allow_ping(ips):
 
 def deny_ping(ips):
     for ip in ips:
-        # Verificar si la regla de aceptación existe
+       
         check_command = f"sudo iptables -C INPUT -p icmp --icmp-type echo-request -s {ip} -j ACCEPT"
         if check_rule_exists(check_command):
-            # Eliminar la regla de aceptación si existe
+
             command = f"sudo iptables -D INPUT -p icmp --icmp-type echo-request -s {ip} -j ACCEPT"
             run_command(command)
         
