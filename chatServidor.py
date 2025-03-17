1d1a8fa54d4a0ca68942e6900133deb387ec17d8
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2025-03-16 21:59:42 -0600

    Update chatServidor.py

--- chatServidor.py
+++ chatServidor.py
@@ -3,7 +3,7 @@ import threading
 import tkinter as tk
 from tkinter import scrolledtext, filedialog, messagebox
 import os
-import struct 
+import struct #importaciones
 
 SERVER_HOST = '172.168.3.104'
 SERVER_PORT = 9685
